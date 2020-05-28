d4ba839147917c0e975a198f83cbb6f4899cb999
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-05-28 17:00:33 +0300

    init brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import userGreeting, { game } from '../src/cli.js';
+
+userGreeting();
+game();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,31 @@
 import readlineSync from 'readline-sync';
 
+const userName = readlineSync.question('May I have your name? ');
 const userGreeting = () => {
-  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!`);
 };
 export default userGreeting;
+
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+export const game = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const numbers = [15, 6, 7];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    const number = numbers[i];
+    const correctAnswer = isEven(number);
+    const answer = readlineSync.question(`Question: ${number}\nYour answer: `);
+    if (answer !== correctAnswer) {
+      return console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
+    }
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};

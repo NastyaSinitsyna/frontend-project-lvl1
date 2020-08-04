e448ce1177b7743dd7dcf76fdb79ba546a74008a
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-08-04 14:15:37 +0300

    fix prime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,18 +3,22 @@ import game from '../index.js';
 
 const isPrime = (n) => {
   if (n < 2) {
-    return 'no';
+    return false;
   }
   const limit = Math.sqrt(n);
   for (let i = 2; i <= limit; i += 1) {
     if (n % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
+
+const sayIfPrime = (num) => (isPrime(num) ? 'yes' : 'no');
+
+const getRandomTillHundred = () => getRandomInt(1, 100);
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const prime = () => game(task, getRandomInt(1, 100), isPrime);
+const prime = () => game(task, getRandomTillHundred, sayIfPrime);
 export default prime;

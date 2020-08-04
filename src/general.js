6347a662fb64330972bff9cab516b3c94725982c
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-08-04 13:52:13 +0300

    fix getRandomInt export

--- src/general.js
+++ src/general.js
@@ -1,6 +1,6 @@
-export const getRandomInt = (floor, ceil) => {
+const getRandomInt = (floor, ceil) => {
   const min = Math.ceil(floor);
   const max = Math.floor(ceil);
   return Math.floor(Math.random() * (max - min)) + min;
 };
-  
+export default getRandomInt;

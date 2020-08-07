dc288ad54883808b94ae805120824e0d417f387f
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-08-07 11:28:46 +0300

    fix even function

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,8 @@
 import getRandomInt from '../general.js';
 import game from '../index.js';
 
+const getRandomToHundred = () => getRandomInt(1, 100);
+
 const isEven = (num) => {
   if (num % 2 === 0) {
     return true;
@@ -8,11 +10,14 @@ const isEven = (num) => {
   return false;
 };
 
-const sayIfEven = (num) => (isEven(num) ? 'yes' : 'no');
-
-const getRandomTillHundred = () => getRandomInt(1, 100);
+const getNumAndSayIfEven = () => {
+  const question = getRandomToHundred();
+  const answer = isEven(question) ? 'yes' : 'no';
+  const result = [question, answer];
+  return result;
+};
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const even = () => game(task, getRandomTillHundred, sayIfEven);
+const even = () => game(task, getNumAndSayIfEven);
 export default even;

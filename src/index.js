595ede86e9e9c85f22eddac15f5d8d5eedd0d626
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-08-07 11:27:39 +0300

    update game function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const game = (task, getQuestion, getCorrectAnswer) => {
+const game = (task, getQuestionWithAnswer) => {
   const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hi, ${userName}!\n${task}`);
   for (let i = 0; i < 3; i += 1) {
-    const question = getQuestion();
-    const correctAnswer = getCorrectAnswer(question);
+    const questionAndAnswer = getQuestionWithAnswer();
+    const question = questionAndAnswer[0];
+    const correctAnswer = questionAndAnswer[1]; // Использовать ли REST????
     const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
     if (answer !== correctAnswer) {
       return console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);

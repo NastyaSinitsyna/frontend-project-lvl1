a490d34b44da9977f1fabd07f87b55cea941ed1f
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-08-07 11:31:14 +0300

    fix progression function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,46 +1,24 @@
 import getRandomInt from '../general.js';
 import game from '../index.js';
 
-const getProgression = () => {
+const getProgressionWithAnswer = () => {
   const firstItem = getRandomInt(1, 100);
   const diff = getRandomInt(1, 100);
-  const coll = [firstItem, firstItem + diff];
+  const coll = [firstItem];
   const collFullLength = 10;
-  const firstAddedItem = coll.length + 1;
-  for (let n = firstAddedItem; n <= collFullLength; n += 1) {
-    const stepNumber = n - 1;
-    coll[stepNumber] = firstItem + (n - 1) * diff;
+  for (let i = 1; i < collFullLength; i += 1) {
+    coll[i] = coll[0] + diff * i;
   }
-  const specialItem = Math.floor(Math.random() * (coll.length - 1));
-  coll[specialItem] = '..';
-  const result = coll.join(' ');
+  const secretItemIndex = getRandomInt(1, collFullLength);
+  const answer = String(coll[secretItemIndex]);
+  coll[secretItemIndex] = '..';
+  const question = coll.join(' ');
+  const result = [question, answer];
   return result;
 };
 
-const getSpecialItem = (progression) => {
-  const coll = progression.split(' ');
-  let diff;
-  let answer;
-  for (let i = 0; i < coll.length; i += 1) {
-    if (coll[i] === '..') {
-      switch (i) {
-        case (0):
-          diff = Number(coll[i + 2]) - Number(coll[i + 1]);
-          break;
-        case (coll.length - 1):
-          diff = Number(coll[i - 2]) - Number(coll[i - 1]);
-          break;
-        default:
-          diff = (Number(coll[i + 1]) - Number(coll[i - 1])) / 2;
-          break;
-      }
-      answer = i === 0 ? Number(coll[i + 1]) - diff : Number(coll[0]) + diff * i;
-    }
-  }
-  return String(answer);
-};
 
 const task = 'What number is missing in the progression?';
 
-const progression = () => game(task, getProgression, getSpecialItem);
+const progression = () => game(task, getProgressionWithAnswer);
 export default progression;

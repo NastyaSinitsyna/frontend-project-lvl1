84837798eaa7b2347982beb7239969d3dc308db1
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-08-13 13:51:31 +0300

    fix calc functions

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,40 +1,28 @@
 import getRandomInt from '../general.js';
 import game from '../index.js';
 
-const getExpression = () => {
-  const num1 = getRandomInt(1, 100);
-  const num2 = getRandomInt(1, 100);
-  const operations = ['+', '-', '*'];
-  const operation = operations[getRandomInt(1, operations.length)];
-  const expression = `${num1} ${operation} ${num2}`;
-  return expression;
-};
-
-const calcStringExp = (string) => {
-  const stringColl = string.split(' ');
-  const num1 = Number(stringColl[0]);
-  const num2 = Number(stringColl[2]);
-  const operator = stringColl[1];
-  let result;
+const calcExp = (num1, num2, operator) => {
   switch (operator) {
     case '+':
-      result = num1 + num2;
-      break;
+      return num1 + num2;
     case '-':
-      result = num1 - num2;
-      break;
+      return num1 - num2;
     case '*':
-      result = num1 * num2;
-      break;
+      return num1 * num2;
     default:
-      return undefined;
+      return false;
   }
-  return result;
 };
 
 const getExpAndCalc = () => {
-  const question = getExpression();
-  const answer = String(calcStringExp(question));
+  const num1 = getRandomInt(1, 100);
+  const num2 = getRandomInt(1, 100);
+  const operators = ['+', '-', '*'];
+  const operator = operators[getRandomInt(1, operators.length)];
+
+  const question = `${num1} ${operator} ${num2}`;
+  const answer = String(calcExp(num1, num2, operator));
+
   const result = [question, answer];
   return result;
 };

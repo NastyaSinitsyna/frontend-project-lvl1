1f423d8fcc2a021d4351a78d5929d34c6fb89526
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-09-22 16:49:21 +0300

    fix playGame function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import readlineSync from 'readline-sync';
 
 const playGame = (task, getQuestionWithAnswer) => {
-  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!\n${task}`);
   const numberOfTries = 3;
+  let message;
   for (let i = 0; i < numberOfTries; i += 1) {
-    let message;
     const [question, correctAnswer] = getQuestionWithAnswer();
     const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
     if (answer !== correctAnswer) {
       message = `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`;
       return console.log(message);
     }
-    message = 'Correct!';
-    console.log(message);
+    console.log('Correct!');
   }
-  const congratulation = `Congratulations, ${userName}!`;
-  return console.log(congratulation);
+  message = `Congratulations, ${userName}!`;
+  return console.log(message);
 };
 export default playGame;

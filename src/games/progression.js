dd88edd50b395a242e321fd78bd4551b40547f9c
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-09-22 16:35:18 +0300

    fix getProgressionGameData func

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,15 +4,15 @@ import playGame from '../index.js';
 const getProgressionGameData = () => {
   const start = getRandomInt(1, 100);
   const diff = getRandomInt(1, 100);
-  const coll = [];
-  const collFullLength = 10;
-  for (let i = 0; i < collFullLength; i += 1) {
+  const progression = [];
+  const progFullLength = 10;
+  for (let i = 0; i < progFullLength; i += 1) {
     coll[i] = start + diff * i;
   }
-  const secretItemIndex = getRandomInt(1, collFullLength);
-  const answer = String(coll[secretItemIndex]);
-  coll[secretItemIndex] = '..';
-  const question = coll.join(' ');
+  const secretItemIndex = getRandomInt(1, progFullLength);
+  const answer = String(progression[secretItemIndex]);
+  progression[secretItemIndex] = '..';
+  const question = progression.join(' ');
   const result = [question, answer];
   return result;
 };

35edbe3ddf6db8e02ffe65390374a1d179df88df
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-09-30 12:03:35 +0300

    fix intGenerator

--- src/intgenerator.js
+++ src/intgenerator.js
@@ -1,6 +1,6 @@
 const getRandomInt = (floor, ceil) => {
   const min = Math.ceil(floor);
   const max = Math.floor(ceil);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 export default getRandomInt;

2d66172c1272506cbd4d48556647a71a6fd45cff
Nastya Sinitsyna <nastasya2507@gmail.com>
2020-09-30 12:04:42 +0300

    fix progression function

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,11 +5,11 @@ const getProgressionGameData = () => {
   const start = getRandomInt(1, 100);
   const diff = getRandomInt(1, 100);
   const progression = [];
-  const progFullLength = 10;
-  for (let i = 0; i < progFullLength; i += 1) {
+  const progressionLength = 10;
+  for (let i = 0; i < progressionLength; i += 1) {
     progression[i] = start + diff * i;
   }
-  const secretItemIndex = getRandomInt(1, progFullLength);
+  const secretItemIndex = getRandomInt(1, progressionLength - 1);
   const answer = String(progression[secretItemIndex]);
   progression[secretItemIndex] = '..';
   const question = progression.join(' ');
